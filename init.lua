7e3579716a20dc880038fce25bf9b585936493e6
windrim <martin.allen@theijf.org>
2023-06-12 12:18:25 -0400

    initial commit

---
+++ init.lua
@@ -0,0 +1,13 @@
+return {
+  plugins = {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.colorscheme.catppuccin" },
+    { import = "astrocommunity.colorscheme.everforest" },
+    { import = "astrocommunity.colorscheme.nord-nvim" },
+    { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+    { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+    { import = "astrocommunity.pack.python" },
+    { import = "astrocommunity.pack.lua" },
+  },
+  colorscheme = "everforest",
+}
