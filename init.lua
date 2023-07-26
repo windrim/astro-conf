0b9decc979f79fa7ea8fb931989d77e89ed6e9fe
windrim <martin.allen@theijf.org>
2023-07-26 16:15:32 -0400

    new colorschemes

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 return {
   plugins = {
+    -- community: color schemes, python pack
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.colorscheme.catppuccin" },
     { import = "astrocommunity.colorscheme.everforest" },
@@ -7,7 +8,11 @@ return {
     { import = "astrocommunity.colorscheme.gruvbox-nvim" },
     { import = "astrocommunity.colorscheme.tokyonight-nvim" },
     { import = "astrocommunity.pack.python" },
+    -- functional
     "christoomey/vim-tmux-navigator",
+    -- colors
+    "blazkowolf/gruber-darker.nvim",
+    "ayu-theme/ayu-vim",
   },
   colorscheme = "everforest",
 }
