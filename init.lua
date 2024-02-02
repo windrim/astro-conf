ce1555ca5dbd7d95ccf5d7aa4bbaa069c211cdf8
windrim <martin.allen@theijf.org>
2024-02-02 16:06:48 -0500

    conf update

--- init.lua
+++ init.lua
@@ -3,13 +3,9 @@ return {
     -- community: python/rust pack(s)
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.pack.python-ruff" },
-    -- functional
-    "christoomey/vim-tmux-navigator",
-    "ggandor/leap.nvim",
+    { import = "astrocommunity.motion.leap-nvim" },
     -- colors
     "blazkowolf/gruber-darker.nvim",
-    "lourenci/github-colors",
-    "fenetikm/falcon"
   },
   colorscheme = "gruber-darker",
 }
